172c4c0bea632baed927d68025867a14fc8b84b1
Gajanan <gajananbhange200@gmail.com>
2024-09-20 17:29:31 +0530

    Test US Shift updated with latest Code

---
+++ src/Framework2/TestUS.java
@@ -0,0 +1,22 @@
+package Framework2;
+
+import org.testng.annotations.Test;
+
+public class TestUS
+
+{
+
+	@Test
+	public void India() 
+	{
+	System.out.println("Im India Test");
+	}
+	
+	@Test
+	public void US() 
+	{
+	System.out.println("Im US Test");
+	}
+	
+}
+
